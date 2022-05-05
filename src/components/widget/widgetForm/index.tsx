67553519f3a196bc524e717704bd3e8bc9e6b67b
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-05-05 20:50:55 -0300

    finishWidgetForm

--- src/components/widget/widgetForm/index.tsx
+++ src/components/widget/widgetForm/index.tsx
@@ -1,17 +1,36 @@
-import { CloseButton } from "../../closeButton"
-import "./style.css"
+import { useState } from "react";
+import { CloseButton } from "../../closeButton";
+import "./style.css";
 
-export function WidgetForm () {
-    return(
-        <div className="widgetFormContainer">
-            <header>
-                <CloseButton />
-                <span className="titleHeader">Deixe seu feedback</span>
-            </header>
-            <p>Hello World</p>
-            <footer className="widgetFormFooter">
-                Feito com ♥︎ pela <a className="linkWidget" href="https://Rocketseat.com.br">Rocketseat</a>
-            </footer>
-        </div>
-    )
+import { feedbackTypes } from "../../../utils/feedbackTypes";
+import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+      setFeedbackType(null)
+  }
+  return (
+    <div className="widgetFormContainer">
+      
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep 
+        onFeedbackRestartRequested={handleRestartFeedback} 
+        feedbackType={feedbackType} />
+      )}
+
+      <footer className="widgetFormFooter">
+        Feito com ♥︎ pela{" "}
+        <a className="linkWidget" href="https://Rocketseat.com.br">
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
 }
