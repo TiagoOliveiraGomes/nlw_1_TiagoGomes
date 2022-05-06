c8ce55729d7466b542f5a7a8b8d5e262ab6e4b80
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-05-06 00:18:00 -0300

    finished screenshotbutton

---
+++ src/components/screenshotButton/index.tsx
@@ -0,0 +1,49 @@
+import { Camera, Trash } from "phosphor-react";
+import html2canvas from 'html2canvas'
+import './style.css'
+import { useState } from "react";
+import { Loading } from "../loading";
+
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton(props:ScreenshotButtonProps) {
+    const {screenshot, onScreenshotTook} = props
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
+
+    async function handleTakeScreenshot () {
+        setIsTakingScreenshot(true)
+
+        const canvas = await html2canvas(document.querySelector('html')!)
+        const base64img = canvas.toDataURL('image/png')
+        onScreenshotTook(base64img)
+
+        setIsTakingScreenshot(false)
+    }
+
+    if(screenshot){
+        console.log("daskdoasd")
+        return (
+            <button
+            type="button"
+            className="btnScreenshotAlternative"
+            onClick={()=> onScreenshotTook(null)}
+            style={{
+                backgroundImage: `url(${screenshot})`,
+                backgroundPosition: 'right bottom',
+                backgroundSize: 180,
+            }}
+            >
+                <Trash weight="fill" />
+            </button>
+        )
+    }
+  return (
+    <button
+    onClick={handleTakeScreenshot} type="button" className="btnScreenshot bg-zinc-800">
+      {isTakingScreenshot? <Loading />: <Camera className="cameraIcon" />}
+    </button>
+  );
+}
